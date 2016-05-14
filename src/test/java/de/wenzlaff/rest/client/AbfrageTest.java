decd87ef8b2077fdc7060b5cf94ab49c1f0f63f5
Thomas Wenzlaff <karotz@wenzlaff.de>
2016-05-14 08:15:51 +0200

    JUnit Test integriert

---
+++ src/test/java/de/wenzlaff/rest/client/AbfrageTest.java
@@ -0,0 +1,18 @@
+package de.wenzlaff.rest.client;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Thomas Wenzlaff
+ * @version 0.1
+ */
+public class AbfrageTest {
+
+	@Test
+	public void testMain() throws Exception {
+		String[] args = null;
+		Abfrage.main(args);
+	}
+
+}
